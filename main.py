cbb512e0bca3374d42bca93ac2ef627b39090c59
Emojigit <55009343+Emojigit@users.noreply.github.com>
2021-09-16 18:54:30 +0800

    Stable!

--- main.py
+++ main.py
@@ -68,6 +68,70 @@ else:
     print("Login failed")
     exit(3)
 
+def WorkOnPage(title):
+    print("Working on {}".format(title))
+    edit_token_data = PerformAPIGetActions(S,{
+        'action':"query",
+        'meta':"tokens",
+        'type':"csrf",
+        'format':"json",
+        'curtimestamp':True,
+    })
+    edit_token = edit_token_data['query']['tokens']['csrftoken']
+    starttimestamp = edit_token_data["curtimestamp"]
+    page_data = PerformAPIGetActions(S,{
+        "action":"query",
+        "prop":"revisions",
+        "titles":title,
+        "rvslots":"*",
+        "rvprop":"content|timestamp",
+        "formatversion":"2",
+        'format':"json"
+    })
+    try:
+        TS = page_data["query"]["pages"][0]["revisions"][0]["timestamp"]
+    except KeyError:
+        print("Page not exist or timestamp invalid in {}".format(title))
+        return
+    page_content = page_data["query"]["pages"][0]["revisions"][0]["slots"]["main"]["content"]
+    page_modifyed_data = page_content
+    for x,y in config["replaces"].items():
+        print("Working on '{}' to '{}'".format(x,y))
+        page_modifyed_data = page_modifyed_data.replace(x,y)
+    
+    PerformAPIPostActions(S,{
+        "action": "edit",
+        "title": title,
+        "token": edit_token,
+        "format": "json",
+        "text": page_modifyed_data,
+        "summary":config["summary"],
+        "bot": True,
+        "headers":{'Content-Type': 'multipart/form-data'},
+        "basetimestamp":TS,
+        "starttimestamp":starttimestamp,
+    })
+    
+
+if len(sys.argv) == 2:
+    print("Working on spec page `{}`".format(sys.argv[1]))
+    WorkOnPage(sys.argv[1])
+else:
+    while True:
+        print("not provide title, do in random pages")
+        pageTitle = PerformAPIGetActions(S,{
+            "action":"query",
+            "list":"random",
+            "rnlimit":1,
+            "utf8":"",
+            "format":"json",
+        })["query"]["random"][0]["title"]
+        print("Working on {}".format(pageTitle))
+        WorkOnPage(pageTitle)
+        time.sleep(config["delay"])
+
+    
+
 
 
 
